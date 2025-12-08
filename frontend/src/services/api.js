557186adf6a289bdf2205971ab2f712d33e99b47
Vdtry-06 <tunhoipro0306@gmail.com>
2025-12-08 19:50:11 +0700

    add url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://26.104.211.44:8080/api";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
